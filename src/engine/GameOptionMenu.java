3db90970c02b9664a166a585d4b735c289aad2df
Jstrong <jstrong1105@naver.com>
2026-02-04 17:31:09 +0900

    옵션 메뉴 약간의 수정

--- src/engine/GameOptionMenu.java
+++ src/engine/GameOptionMenu.java
@@ -3,41 +3,41 @@ package engine;
 import domain.base.GameOptionTemplate;
 import util.InputHandler;
 
-class GameOptionMenu<T , E extends Enum<E> & GameOptionTemplate>
+class GameOptionMenu<T , E extends Enum<E> & GameOptionTemplate<T>>
 {
-	GameOptionMenu(T option, E menu, E[] menuList)
+	GameOptionMenu(T option, E[] menuList)
 	{
 		this.option = option;
-		this.menu = menu;
 		this.menuList = menuList;
 	}
 	
-	private T option;
+	private final T option;
 	
-	private E menu;
-	
-	private E[] menuList;
+	private final E[] menuList;
 	
 	void setOption(String gameName)
 	{
-		System.out.printf("====%s====\n",gameName);
-		
-		for(E list : menuList)
+		while(true)
 		{
-			System.out.println(list.ordinal()+1 + ". " + list.getName() + " : " + list.getExplain());
-		}
-		
-		int answer = InputHandler.readIntRange("변경할 옵션을 선택해주세요.(1~"+menuList.length+") : ", 1,menuList.length);
-		
-		answer--;
-		
-		for(E list : menuList)
-		{
-			if(list.ordinal() == answer)
+			System.out.printf("====%s====\n",gameName);
+			
+			System.out.println("0. 뒤로가기");
+			
+			for(E list : menuList)
 			{
-				list.setOption(option);
+				System.out.println(list.ordinal()+1 + ". " + list.getName() + " : " + list.getExplain());
+			}
+			
+			int answer = InputHandler.readIntRange("변경할 옵션을 선택해주세요.(0~"+menuList.length+") : ", 0,menuList.length);
+			
+			if(answer == 0)
+			{
 				break;
 			}
+			
+			answer--;
+			
+			menuList[answer].setOption(option);
 		}
 	}
 }
